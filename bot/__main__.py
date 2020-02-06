564a617de2969f3aabbf81c1cd0ac31a85ed4e3f
iswenzz <suxlolz1528@gmail.com>
2020-02-06 01:01:03 +0100

    edited main func

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,8 +1,12 @@
 from bot.reportbot import ReportBOT
 import os
 
-if (__name__ == "__main__"):
+def main():
     if os.path.exists("token"):
         with open("token", "r") as f:
             client = ReportBOT()
             client.run(f.read())
+
+
+if (__name__ == "__main__"):
+    main()
